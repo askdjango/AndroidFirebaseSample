a7d2872ab9af4420afc97707078574bb511eb7af
Chinseok Lee <allieuslee@gmail.com>
2016-12-02 21:57:32 +0900

    Firebase 노티를 받아, Toast 메세지를 띄움

---
+++ app/src/main/java/kr/festi/androidfirebasesample/MyFirebaseMessagingService.java
@@ -0,0 +1,41 @@
+package kr.festi.androidfirebasesample;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    static final String TAG = MyFirebaseMessagingService.class.getName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        // 앱이 실행 중일 때 (Foreground 상황) 에서 푸쉬를 받으면 호출됩니다.
+        // 백그라운드 상황에서는 호출되지 않고 그냥 알림목록에 알림이 추가됩니다.
+
+        if ( remoteMessage.getData().size() > 0 ) {
+            Log.d(TAG, "FCM Data Message : " + remoteMessage.getData());
+        }
+
+        if ( remoteMessage.getNotification() != null ) {
+            final String messageBody = remoteMessage.getNotification().getBody();
+
+            Log.d(TAG, "FCM Notification Message Body : " + messageBody);
+
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), messageBody, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
+
