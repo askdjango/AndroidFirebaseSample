1cb5f776fe889a91b60b2242db31925f8aa8d782
Chinseok Lee <allieuslee@gmail.com>
2016-12-02 15:10:43 +0900

    구글로그인을 Firebase Auth와 연동

--- app/src/main/java/kr/festi/androidfirebasesample/MainActivity.java
+++ app/src/main/java/kr/festi/androidfirebasesample/MainActivity.java
@@ -1,16 +1,28 @@
 package kr.festi.androidfirebasesample;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     FirebaseAuth mFirebaseAuth;
     FirebaseUser mFirebaseUser;
+
+    GoogleApiClient mGoogleApiClient;
 
     String mUsername;
     String mPhotoUrl;
@@ -22,17 +34,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.logout_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage("Signout ?")
+                        .setPositiveButton("signout", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                mFirebaseAuth.signOut();
+                                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+
+                                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }).show();
+            }
+        });
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
         if ( mFirebaseUser == null ) {
-            // FIXME: 로그인이 필요합니다.
             Toast.makeText(this, "로그인이 필요합니다", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+            finish();
         }
         else {
             mUsername = mFirebaseUser.getDisplayName();
             if ( mFirebaseUser.getPhotoUrl() != null ) {
                 mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
             }
+
+            TextView usernameTextView = (TextView) findViewById(R.id.username_textview);
+            usernameTextView.setText(mUsername);
+
+            Toast.makeText(this, mUsername + "님 환영합니다.", Toast.LENGTH_SHORT).show();
+
+            // ImageView photoImageView = (ImageView) findViewById(R.id.photo_imageview);
         }
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
     }
 }
---
+++ app/src/main/java/kr/festi/androidfirebasesample/SignInActivity.java
@@ -0,0 +1,118 @@
+package kr.festi.androidfirebasesample;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    FirebaseAuth mFirebaseAuth;
+    FirebaseAuth.AuthStateListener mFirebaseAuthListener;
+
+    SignInButton mSigninGoogleButton;
+    GoogleApiClient mGoogleApiClient;
+
+    static final String TAG = SignInActivity.class.getName();
+    static final int RC_GOOGLE_SIGN_IN = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        setTitle("Login");
+
+        /*
+         * Firebase
+         */
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if ( user != null ) {
+                    Log.d(TAG, "sign in");
+
+                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Log.d(TAG, "sign out");
+                }
+
+            }
+        };
+
+        /*
+         *  Google Login
+         */
+        mSigninGoogleButton = (SignInButton) findViewById(R.id.sign_in_google_button);
+        mSigninGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN);
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mFirebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if ( mFirebaseAuthListener != null )
+            mFirebaseAuth.removeAuthStateListener(mFirebaseAuthListener);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ( requestCode == RC_GOOGLE_SIGN_IN ) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if ( result.isSuccess() ) {
+                String token = result.getSignInAccount().getIdToken();
+                AuthCredential credential = GoogleAuthProvider.getCredential(token, null);
+                mFirebaseAuth.signInWithCredential(credential);
+            }
+            else {
+                Log.d(TAG, "Google Login Failed." + result.getStatus());
+            }
+        }
+    }
+}
